eb4d67b136e089db07c4b9eee0dc8b780a486f63
rk-techs <ryskdevelop@gmail.com>
2023-10-16 08:02:30 +0900

    set up vite config

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -3,8 +3,9 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    
-    @vite('resources/js/app.js')
+
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
     @inertiaHead
   </head>
   <body>
