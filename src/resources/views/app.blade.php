4b5de95dd27e7876721073e4d435d9c1fe8fb1b4
rk-techs <ryskdevelop@gmail.com>
2023-10-19 07:40:27 +0900

    chore: install ziggy
    * composer require tightenco/ziggy

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
+    @routes
     @viteReactRefresh
     @vite(['resources/js/app.jsx', 'resources/sass/app.scss'])
     @inertiaHead
