05f38d1fdf6f093efcda7f8e6ce31c2f38c943ea
rk-techs <ryskdevelop@gmail.com>
2023-11-07 19:55:46 +0900

    リレーション定義

--- src/app/Models/LogisticsAddress.php
+++ src/app/Models/LogisticsAddress.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LogisticsAddress extends Model
 {
@@ -19,4 +20,15 @@ class LogisticsAddress extends Model
         'tel',
         'note',
     ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
