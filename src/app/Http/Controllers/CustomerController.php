7e6c3e2e45911ac8e508688d4355c07c8bcf5fef
rk-techs <ryskdevelop@gmail.com>
2023-11-11 10:38:45 +0900

    PurchaseTermとSalesTermは値がない時はcreateしない

--- src/app/Http/Controllers/CustomerController.php
+++ src/app/Http/Controllers/CustomerController.php
@@ -55,23 +55,27 @@ class CustomerController extends Controller
                 'created_by_id'     => auth()->user()->id,
             ])->save();
 
-            PurchaseTerm::create([
-                'customer_id'           => $customer->id,
-                'billing_type'          => $request->input('purchase_billing_type'),
-                'cutoff_day'            => $request->input('purchase_cutoff_day'),
-                'payment_month_offset'  => $request->input('purchase_payment_month_offset'),
-                'payment_day'           => $request->input('purchase_payment_day'),
-                'payment_day_offset'    => $request->input('purchase_payment_day_offset'),
-            ]);
+            if ($request->input('purchase_billing_type') !== null) {
+                PurchaseTerm::create([
+                    'customer_id'           => $customer->id,
+                    'billing_type'          => $request->input('purchase_billing_type'),
+                    'cutoff_day'            => $request->input('purchase_cutoff_day'),
+                    'payment_month_offset'  => $request->input('purchase_payment_month_offset'),
+                    'payment_day'           => $request->input('purchase_payment_day'),
+                    'payment_day_offset'    => $request->input('purchase_payment_day_offset'),
+                ]);
+            }
 
-            SalesTerm::create([
-                'customer_id'           => $customer->id,
-                'billing_type'          => $request->input('sales_billing_type'),
-                'cutoff_day'            => $request->input('sales_cutoff_day'),
-                'payment_month_offset'  => $request->input('sales_payment_month_offset'),
-                'payment_day'           => $request->input('sales_payment_day'),
-                'payment_day_offset'    => $request->input('sales_payment_day_offset'),
-            ]);
+            if ($request->input('sales_billing_type') !== null) {
+                SalesTerm::create([
+                    'customer_id'           => $customer->id,
+                    'billing_type'          => $request->input('sales_billing_type'),
+                    'cutoff_day'            => $request->input('sales_cutoff_day'),
+                    'payment_month_offset'  => $request->input('sales_payment_month_offset'),
+                    'payment_day'           => $request->input('sales_payment_day'),
+                    'payment_day_offset'    => $request->input('sales_payment_day_offset'),
+                ]);
+            }
         });
 
         return to_route('customers.index')
