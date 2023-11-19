37ba6ad9feb5300f9e3f909c4116df2784b857d9
rk-techs <ryskdevelop@gmail.com>
2023-11-19 23:43:43 +0900

    Inquiry Create, EditのTableInputRowコンポーネント化

---
+++ src/resources/js/Components/TableGenericSelectRow.jsx
@@ -0,0 +1,29 @@
+export default function TableGenericSelectRow({ label, name, data, setData, errors, options }) {
+  return (
+    <tr className="table-row is-flexible">
+      <th className="th-cell">
+        <label htmlFor={name} className="form-label">
+          {label}
+        </label>
+      </th>
+      <td className="td-cell u-flex">
+        <select
+          name={name}
+          id={name}
+          value={data[name]}
+          onChange={e => setData(name, e.target.value)}
+          className={`input-field ${errors[name] ? 'is-invalid' : ''}`}
+        >
+          <option value="">-- {label}を選択 --</option>
+          {options.map((option) => (
+            <option key={option.id} value={option.id}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+        {errors[name] && (<div className="invalid-feedback">{errors[name]}</div>)}
+      </td>
+    </tr>
+  );
+}
+
---
+++ src/resources/js/Components/TableSelectRow.jsx
@@ -0,0 +1,27 @@
+import OptionsList from './OptionsList';
+
+export default function TableSelectRow({ label, name, data, errors, setData, options, isRequired = false }) {
+  return (
+    <tr className="table-row is-flexible">
+      <th className="th-cell">
+      <label htmlFor={name} className="form-label">
+        {label}
+        {isRequired && <span className="required-mark">必須</span>}
+      </label>
+      </th>
+      <td className="td-cell">
+      <select
+        name={name}
+        id={name}
+        value={data[name]}
+        onChange={e => setData(name, e.target.value)}
+        className={`input-field ${errors[name] ? 'is-invalid' : ''}`}
+      >
+        <option value=""></option>
+        <OptionsList options={options} />
+      </select>
+      {errors[name] && <div className="invalid-feedback">{errors[name]}</div>}
+      </td>
+    </tr>
+  );
+}
