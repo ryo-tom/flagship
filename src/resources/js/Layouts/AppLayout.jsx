a6af870b4d61bbe8da9c6f955b698a5edd8a9d2d
rk-techs <ryskdevelop@gmail.com>
2023-11-19 14:45:16 +0900

    mobile-headerレイアウト追加

--- src/resources/js/Layouts/AppLayout.jsx
+++ src/resources/js/Layouts/AppLayout.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Sidebar from './Partials/Sidebar';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 
 export default function AppLayout({ children }) {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
@@ -10,6 +12,15 @@ export default function AppLayout({ children }) {
 
   return (
     <div className="page-wrapper">
+      <header className="mobile-header">
+        <button className="nav-toggler">
+          <MenuIcon className="header-menu-icon" />
+        </button>
+        <div className="header-title">Sales Manager+</div>
+        <button className="header-account-nav">
+          <AccountCircleOutlinedIcon className="header-account-icon" />
+        </button>
+      </header>
       <Sidebar isOpen={isSidebarOpen} onToggle={toggleSidebar} />
       <main className={`main-container ${!isSidebarOpen ? 'is-expanded' : ''}`}>
         {children}
