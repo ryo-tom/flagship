0248c8346c06249ccb36bd958edc26d46ee82d4d
rk-techs <ryskdevelop@gmail.com>
2023-12-02 11:02:10 +0900

    取引先Showページに営業履歴リストを表示

--- src/resources/js/Pages/Customer/Show.jsx
+++ src/resources/js/Pages/Customer/Show.jsx
@@ -248,6 +248,42 @@ const Show = ({ customer, userOptions, deliveryAddressTypes }) => {
         </div>
       </div>
 
+      <div className="content-section">
+        <div className="content-section-title">
+          営業履歴
+        </div>
+        <div className="table-wrapper is-scrollable">
+          <table className="table">
+            <thead className="table-header is-sticky">
+              <tr className="table-row">
+                <th className="th-cell col-fixed">ID</th>
+                <th className="th-cell u-min-w-120">営業担当</th>
+                <th className="th-cell u-min-w-136">連絡日</th>
+                <th className="th-cell u-min-w-120">連絡先</th>
+                <th className="th-cell">提案内容</th>
+                <th className="th-cell">反応</th>
+                <th className="th-cell">備考</th>
+              </tr>
+            </thead>
+            <tbody className="table-body">
+              {contacts.map(contact => (
+                contact.sales_activities.map(activity => (
+                  <tr key={activity.id} className="table-row is-hoverable">
+                    <td className="td-cell col-fixed">{activity.id}</td>
+                    <td className="td-cell">{activity.in_charge_user.name}</td>
+                    <td className="td-cell">{activity.contact_date}</td>
+                    <td className="td-cell">{contact.name}</td>
+                    <td className="td-cell">{activity.proposal}</td>
+                    <td className="td-cell">{activity.feedback}</td>
+                    <td className="td-cell">{activity.note}</td>
+                  </tr>
+                ))
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
     </>
   );
 }
