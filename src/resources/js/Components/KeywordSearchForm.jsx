a863ba140c67be5f4e8a65daa8ccddfe00f5ec0e
rk-techs <ryskdevelop@gmail.com>
2023-12-10 18:15:58 +0900

    ui: improve KeywordSearchForm layouts

--- src/resources/js/Components/KeywordSearchForm.jsx
+++ src/resources/js/Components/KeywordSearchForm.jsx
@@ -1,7 +1,14 @@
+import SearchIcon from '@mui/icons-material/Search';
+
 export default function KeywordSearchForm({ placeholder, data, setData, errors, submit }) {
   return (
     <form onSubmit={submit}>
       <div className="keyword-search-box">
+        <div className="search-icon-wrapper">
+          <SearchIcon
+            style={{ fontSize: '1.25rem', color: '#888' }}
+          />
+        </div>
         <input
           type="search"
           name="keyword"
@@ -10,7 +17,6 @@ export default function KeywordSearchForm({ placeholder, data, setData, errors, 
           className={`input-field ${errors.keyword ? 'is-invalid' : ''}`}
           placeholder={placeholder}
         />
-        <button className="btn btn-secondary">検索</button>
       </div>
       {errors.keyword && <div className="invalid-feedback">{errors.keyword}</div>}
     </form>
