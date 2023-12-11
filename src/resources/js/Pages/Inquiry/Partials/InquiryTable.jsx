229018538043d1f2200af10784aeebe84be8b0cb
rk-techs <ryskdevelop@gmail.com>
2023-12-12 08:19:58 +0900

    ui: Inquiry/Indexレイアウト調整

--- src/resources/js/Pages/Inquiry/Partials/InquiryTable.jsx
+++ src/resources/js/Pages/Inquiry/Partials/InquiryTable.jsx
@@ -1,17 +1,20 @@
 import { Link } from '@inertiajs/react';
 
+import IconButton from '@mui/material/IconButton';
+import EditIcon from '@mui/icons-material/Edit';
+
 export default function InquiryTable({ inquiries }) {
   return (
     <div className="table-wrapper is-scrollable">
       <table className="table">
         <thead className="table-header is-sticky">
           <tr className="table-row">
-            <th className="th-cell u-min-w-72 col-fixed">編集</th>
-            <th className="th-cell u-min-w-64">ID</th>
+            <th className="th-cell u-w-64 u-text-center col-fixed">編集</th>
+            <th className="th-cell u-min-w-64 u-text-center">No.</th>
             <th className="th-cell u-min-w-136">問い合わせ日</th>
-            <th className="th-cell u-min-w-80">対応者</th>
+            <th className="th-cell u-min-w-160">対応者</th>
             <th className="th-cell u-min-w-120">ステータス</th>
-            <th className="th-cell u-min-w-240" colSpan={2}>顧客情報</th>
+            <th className="th-cell u-min-w-320" colSpan={2}>顧客情報</th>
             <th className="th-cell u-min-w-240" colSpan={2}>商品情報</th>
             <th className="th-cell u-min-w-120">区分</th>
             <th className="th-cell">件名</th>
@@ -20,12 +23,14 @@ export default function InquiryTable({ inquiries }) {
         <tbody className="table-body">
           {inquiries.map(inquiry => (
             <tr key={inquiry.id} className="table-row is-hoverable">
-              <td className="td-cell col-fixed">
-                <Link href={route('inquiries.edit', inquiry)} className="link">
-                  編集
+              <td className="td-cell col-fixed u-text-center">
+                <Link href={route('inquiries.edit', inquiry)}>
+                  <IconButton size="small" aria-label="edit">
+                    <EditIcon color="primary" />
+                  </IconButton>
                 </Link>
               </td>
-              <td className="td-cell">{inquiry.id}</td>
+              <td className="td-cell u-text-center">{inquiry.id}</td>
               <td className="td-cell">{inquiry.inquiry_date}</td>
               <td className="td-cell">{inquiry.in_charge_user.name}</td>
               <td className="td-cell">{inquiry.status_label}</td>
