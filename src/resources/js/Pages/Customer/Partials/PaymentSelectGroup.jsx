1be111f5f2082aa07ba4c0221a055595c542b131
rk-techs <ryskdevelop@gmail.com>
2023-12-20 21:11:16 +0900

    feat: 取引先支払条件デフォルトセット,クリア機能追加

--- src/resources/js/Pages/Customer/Partials/PaymentSelectGroup.jsx
+++ src/resources/js/Pages/Customer/Partials/PaymentSelectGroup.jsx
@@ -1,26 +1,79 @@
+import ClearAllIcon from '@mui/icons-material/ClearAll';
+import IconButton from '@mui/material/IconButton';
 import OptionsList from '@/Components/OptionsList';
 
 export default function PaymentSelectGroup({ data, setData, errors, paymentTermOptions, transactionType }) {
 
   const prefix = transactionType ? `${transactionType}_` : '';
 
-  const billingType         = `${prefix}billing_type`;
-  const cutoffDay           = `${prefix}cutoff_day`;
-  const paymentMonthOffset  = `${prefix}payment_month_offset`;
-  const paymentDay          = `${prefix}payment_day`;
-  const paymentDayOffset    = `${prefix}payment_day_offset`;
+  const billingType = `${prefix}billing_type`;
+  const cutoffDay = `${prefix}cutoff_day`;
+  const paymentMonthOffset = `${prefix}payment_month_offset`;
+  const paymentDay = `${prefix}payment_day`;
+  const paymentDayOffset = `${prefix}payment_day_offset`;
+
+  function getDefaultTerm() {
+    if (transactionType === 'purchase') {
+      return {
+        [billingType]: 1,
+        [cutoffDay]: 99,
+        [paymentMonthOffset]: 1,
+        [paymentDay]: 99,
+      };
+    } else if (transactionType === 'sales') {
+      return {
+        [billingType]: 2,
+        [paymentDayOffset]: 0,
+      };
+    }
+  }
+
+  function handleClickDefaultTerms() {
+    const defaultTerm = getDefaultTerm();
+
+    setData({
+      ...data,
+      ...defaultTerm,
+    })
+  }
+
+  function handleClickClear() {
+    setData({
+      ...data,
+      [billingType]: '',
+      [cutoffDay]: '',
+      [paymentMonthOffset]: '',
+      [paymentDay]: '',
+      [paymentDayOffset]: '',
+    })
+  }
 
   return (
     <div className="">
-      <SelectInput
-        name={billingType}
-        label="..."
-        value={data[billingType]}
-        onChange={e => setData(billingType, e.target.value)}
-        error={errors[billingType]}
-        options={paymentTermOptions.billingTypes}
-        className="u-w-120"
-      />
+      <div className="u-flex">
+        <SelectInput
+          name={billingType}
+          label="..."
+          value={data[billingType]}
+          onChange={e => setData(billingType, e.target.value)}
+          error={errors[billingType]}
+          options={paymentTermOptions.billingTypes}
+          className="u-w-120 u-mr-2"
+        />
+        <button
+          type="button"
+          className="btn btn-secondary"
+          style={{ padding: '4px', fontSize: '0.875rem' }}
+          onClick={handleClickDefaultTerms}
+        >
+          デフォルト
+        </button>
+        {data[billingType] && (
+          <IconButton size="small" aria-label="edit" onClick={handleClickClear}>
+            <ClearAllIcon />
+          </IconButton>
+        )}
+      </div>
       {data[billingType] == 1 && (
         <>
           <div className="u-flex u-items-center u-my-2">
