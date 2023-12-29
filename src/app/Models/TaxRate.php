3553a0999fd5228643f1e760fb087ee8dd349e2a
rk-techs <ryskdevelop@gmail.com>
2023-12-29 11:05:41 +0900

    現在の税率取得, share dateでフロントへ共有

--- src/app/Models/TaxRate.php
+++ src/app/Models/TaxRate.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class TaxRate extends Model
 {
     use HasFactory;
+
+    public static function getCurrentTaxRate(): TaxRate
+    {
+        return self::where('start_date', '<=', today())
+            ->where(function ($query) {
+                $query->whereNull('end_date')
+                    ->orWhere('end_date', '>=', today());
+            })
+            ->orderBy('start_date', 'desc')
+            ->first();
+    }
 }
