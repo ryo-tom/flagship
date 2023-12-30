0a5ae3e295e53307763484299dcb0a84e499806a
rk-techs <ryskdevelop@gmail.com>
2023-12-30 09:08:53 +0900

    feat: SalesOrder/Show  請求条件の表示

---
+++ src/resources/js/Pages/SalesOrder/Partials/TermDetails.jsx
@@ -0,0 +1,20 @@
+export default function TermDetails({ salesOrder }) {
+  const termLabels = salesOrder.sales_term_labels;
+  switch (salesOrder?.billing_type) {
+    case 1:
+      return (
+        <>
+          <span className="u-mr-3">{termLabels?.cutoff_day}締め</span>
+          <span>{termLabels?.payment_month_offset}{termLabels?.payment_day}払い</span>
+        </>
+      );
+    case 2:
+      return (
+        <>
+          <span>{termLabels?.payment_day_offset}</span>
+        </>
+      );
+    default:
+      return null;
+  }
+};
