00d9151b86c9b51394e092a820d89b296d45916e
rk-techs <ryskdevelop@gmail.com>
2024-01-09 20:21:38 +0900

    feat(ui): 受注一覧 明細td要素内に擬似tableレイアウト (#60)
    * feat(ui): td要素内に擬似テーブルスタイル追加
    
    * feat(ui): inner-thスタイルを追加
    
    * refactor(ui): table行下線をtdからtrに変更
    
    * border-collapseをcollapseにする
    * collapseの影響で固定tdのborderが消えるため擬似要素で表現
    
    * feat(ui): 複数行trの強調borderスタイル

--- src/resources/js/Components/ClickableRow.jsx
+++ src/resources/js/Components/ClickableRow.jsx
@@ -1,13 +1,14 @@
 import { router } from '@inertiajs/react'
 
-export default function ClickableRow({ url, children }) {
+export default function ClickableRow({ url, children, className = '' }) {
   const handleClick = () => {
     router.get(url);
   };
 
   return (
-    <tr onClick={handleClick} className="table-row is-clickable">
+    <tr onClick={handleClick} className={`table-row is-clickable ${className}`}>
       {children}
     </tr>
   );
 }
+
--- src/resources/js/Pages/SalesOrder/Partials/SalesOrderTable.jsx
+++ src/resources/js/Pages/SalesOrder/Partials/SalesOrderTable.jsx
@@ -12,13 +12,21 @@ export default function SalesOrderTable({ salesOrders }) {
             <th className="th-cell u-min-w-240">販売先</th>
             <th className="th-cell u-min-w-120">販売担当</th>
             <th className="th-cell u-min-w-120">合計金額</th>
-            <th className="th-cell u-min-w-280">明細</th>
+            <th className="th-cell contains-table">
+              <div className="inner-thead">
+                <div className="inner-tr">
+                  <div className="inner-th u-w-64">No</div>
+                  <div className="inner-th u-w-160">商品</div>
+                  <div className="inner-th u-w-200">価格</div>
+                </div>
+              </div>
+            </th>
             <th className="th-cell">備考</th>
           </tr>
         </thead>
         <tbody className="table-body">
           {salesOrders.map(salesOrder => (
-            <ClickableRow key={salesOrder.id} url={route('sales-orders.show', salesOrder)}>
+            <ClickableRow key={salesOrder.id} url={route('sales-orders.show', salesOrder)} className="emphasized-row">
               <td className="td-cell col-fixed">{salesOrder.id}</td>
               <td className="td-cell">{salesOrder.delivery_date}</td>
               <td className="td-cell">{salesOrder.product_category.name}</td>
@@ -28,10 +36,16 @@ export default function SalesOrderTable({ salesOrders }) {
                 {salesOrder.display_total} <br />
                 ({salesOrder.display_total_with_tax})
               </td>
-              <td className="td-cell">
-                {salesOrder.sales_order_details.map(detail => (
-                  <div key={detail.id}>{detail.row_number} : {detail.product_name} {detail.display_price}円</div>
-                ))}
+              <td className="td-cell contains-table">
+                <div className="inner-tbody">
+                  {salesOrder.sales_order_details.map(detail => (
+                    <div key={detail.id} className="inner-tr">
+                      <div className="inner-td u-w-64">{detail.row_number}</div>
+                      <div className="inner-td u-w-160">{detail.product_name}</div>
+                      <div className="inner-td u-w-200">{detail.display_price}円</div>
+                    </div>
+                  ))}
+                </div>
               </td>
               <td className="td-cell u-ellipsis u-max-w-200">{salesOrder.note}</td>
             </ClickableRow>
