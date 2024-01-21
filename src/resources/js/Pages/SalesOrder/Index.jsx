eb09ab374e611c56a8c28854a07252fa8ff4cb27
rk-techs <ryskdevelop@gmail.com>
2024-01-21 16:01:48 +0900

    SalesOrderFilter

--- src/resources/js/Pages/SalesOrder/Index.jsx
+++ src/resources/js/Pages/SalesOrder/Index.jsx
@@ -5,15 +5,12 @@ import AppLayout from '@/Layouts/AppLayout';
 import Alert from '@/Components/Alert';
 import Pagination from '@/Components/Pagination';
 import KeywordSearchForm from '@/Components/KeywordSearchForm';
-import SalesOrderTable from "./Partials/SalesOrderTable";
-
-import CustomSelect from '@/Components/Form/CustomSelect';
+import SalesOrderTable from './Partials/SalesOrderTable';
 import DateRangePicker from '@/Components/DateRangePicker';
-import FormLabel from '@/Components/Form/FormLabel';
-import Input from '@/Components/Form/Input';
 import ToggleFilterButton from '@/Components/ToggleFilterButton';
 import { formatCurrency } from '@/Utils/priceCalculator';
-import FilterApplyButton from '@/Components/FilterApplyButton';
+import FilterForm from '@/Components/FilterForm';
+import SalesOrderFilter from './Partials/SalesOrderFilter';
 
 const Index = ({ salesOrders, userOptions, productCategoryOptions, totals }) => {
   const urlParams = route().params;
@@ -38,21 +35,6 @@ const Index = ({ salesOrders, userOptions, productCategoryOptions, totals }) => 
     start_date: urlParams.start_date || '',
     end_date: urlParams.end_date || '',
   });
-
-  function resetSearchInputs() {
-    setData({
-      ...data,
-      keyword: '',
-      product_category_id: '',
-      product_name: '',
-      product_detail: '',
-      customer_name: '',
-      sales_in_charge_id: '',
-      consignee: '',
-      start_date: '',
-      end_date: '',
-    })
-  }
 
   function submit(e) {
     e.preventDefault();
@@ -138,98 +120,16 @@ const Index = ({ salesOrders, userOptions, productCategoryOptions, totals }) => 
         <Pagination paginator={salesOrders} />
       </div>
 
-      <form onSubmit={submit}>
-        <div className={`filter-section ${isFilterOpen ? 'show' : ''}`}>
-          <div className="filter-form-body">
-            <div className="u-mr-2 u-w-240">
-              <FormLabel label="商品カテゴリ" />
-              <CustomSelect
-                onChange={value => setData('product_category_id', value)}
-                options={productCategoryOptions}
-                value={data.product_category_id}
-                valueKey="id"
-                labelKey="name"
-                isClearable={true}
-                isSearchable={true}
-                placeholder="..."
-                error={errors.product_category_id}
-              />
-            </div>
-
-            <div className="u-mr-2 u-w-200">
-              <FormLabel htmlFor="product_name" label="商品名" />
-              <Input
-                id="product_name"
-                type="text"
-                value={data.product_name}
-                onChange={e => setData('product_name', e.target.value)}
-                error={errors.product_name}
-              />
-            </div>
-
-            <div className="u-mr-2 u-w-200">
-              <FormLabel htmlFor="product_detail" label="仕様" />
-              <Input
-                id="product_detail"
-                type="text"
-                value={data.product_detail}
-                onChange={e => setData('product_detail', e.target.value)}
-                error={errors.product_detail}
-              />
-            </div>
-
-            <div className="u-mr-2 u-w-200">
-              <FormLabel htmlFor="customer_name" label="販売先名" />
-              <Input
-                id="customer_name"
-                type="text"
-                value={data.customer_name}
-                onChange={e => setData('customer_name', e.target.value)}
-                error={errors.customer_name}
-              />
-            </div>
-
-            <div className="u-mr-2 u-w-168">
-              <FormLabel label="受注担当" />
-              <CustomSelect
-                onChange={value => setData('sales_in_charge_id', value)}
-                options={userOptions}
-                value={data.sales_in_charge_id}
-                valueKey="id"
-                labelKey="name"
-                isClearable={true}
-                isSearchable={true}
-                placeholder="..."
-                error={errors.sales_in_charge_id}
-              />
-            </div>
-
-            <div className="u-mr-2 u-w-200">
-              <FormLabel htmlFor="consignee" label="納品先" />
-              <Input
-                id="consignee"
-                type="text"
-                value={data.consignee}
-                onChange={e => setData('consignee', e.target.value)}
-                error={errors.consignee}
-                placeholder="住所/会社名"
-              />
-            </div>
-
-          </div>
-          <div className="filter-form-footer">
-            <FilterApplyButton handleClick={submit} style={{ marginRight: '16px' }} />
-            <Link
-              href={route('sales-orders.index')}
-              className="btn btn-secondary"
-              preserveState={true}
-              onSuccess={resetSearchInputs}
-            >
-              クリア
-            </Link>
-          </div>
-        </div>
-      </form>
+      <FilterForm submit={submit} isFilterOpen={isFilterOpen}>
+        <SalesOrderFilter
+          submit={submit}
+          data={data}
+          setData={setData}
+          errors={errors}
+          userOptions={userOptions}
+          productCategoryOptions={productCategoryOptions}
+        />
+      </FilterForm>
 
       <Alert type={flash.type} message={flash.message} />
 
