146c80e0707fcdd7b942f0f5b9e2793ba4af5c65
rk-techs <ryskdevelop@gmail.com>
2024-01-26 21:21:35 +0900

    ui: improve PageSizeSelector

--- src/resources/js/Components/PageSizeSelector.jsx
+++ src/resources/js/Components/PageSizeSelector.jsx
@@ -4,11 +4,12 @@ function PageSizeSelector({ pageSize, onChange,  }) {
 
   return (
     <div className="u-flex u-items-center u-ml-auto u-mr-2">
-      <div className="u-text-sm u-min-w-64">表示件数</div>
+      <div className="u-text-sm u-text-center u-min-w-64">表示件数</div>
       <select
         value={pageSize}
         onChange={onChange}
         className="form-select u-min-w-88"
+        style={{ height: '32px'}}
       >
         <OptionsList
           options={[
