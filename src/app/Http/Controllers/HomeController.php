5350e7bdb2d405f44041ae0ec3ef92f2119ba96c
rk-techs <ryskdevelop@gmail.com>
2024-01-28 13:32:51 +0900

    inquiriesのstatus毎件数を表示

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Inquiry;
+use Carbon\Carbon;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -9,6 +11,30 @@ class HomeController extends Controller
 {
     public function index(): Response
     {
-        return Inertia::render('Home');
+        $currentMonthRange = $this->getCurrentMonthRange();
+        $inquiriesCountByStatus = $this->getInquiriesCountByStatus($currentMonthRange);
+        $totalInquiriesCount = Inquiry::whereBetween('inquiry_date', $currentMonthRange)->count();
+
+        return Inertia::render('Home', compact(
+            'inquiriesCountByStatus',
+            'totalInquiriesCount',
+        ));
+    }
+
+    private function getInquiriesCountByStatus(array $dateTimeRange)
+    {
+        return Inquiry::whereBetween('inquiry_date', $dateTimeRange)
+                ->selectRaw('status, COUNT(*) as count')
+                ->groupBy('status')
+                ->get();
+    }
+
+    private function getCurrentMonthRange(): array
+    {
+        $currentDate = Carbon::today();
+        $firstDayOfCurrentMonth = $currentDate->copy()->startOfMonth()->startOfDay();
+        $lastDayOfCurrentMonth  = $currentDate->copy()->endOfMonth()->endOfDay();
+        
+        return [$firstDayOfCurrentMonth, $lastDayOfCurrentMonth];
     }
 }
--- src/resources/js/Pages/Home.jsx
+++ src/resources/js/Pages/Home.jsx
@@ -1,10 +1,26 @@
 import AppLayout from '@/Layouts/AppLayout'
 
-const Home = () => {
+const Home = ({ inquiriesCountByStatus, totalInquiriesCount }) => {
   return (
     <>
       <h1>HOME</h1>
       <p>Welcome to Sales Manager+</p>
+
+
+      <div className="u-my-4">
+        <div>当月 問い合わせ状況</div>
+        {inquiriesCountByStatus.map(inquiry => (
+          <p key={inquiry.status}>
+            <span className={`inquiry-status status-${inquiry.status} u-mr-1`}>
+              {inquiry.status_label}
+            </span>
+            <span>
+              : {inquiry.count}件
+            </span>
+          </p>
+        ))}
+        <div>合計 : {totalInquiriesCount}件</div>
+      </div>
     </>
   );
 }
