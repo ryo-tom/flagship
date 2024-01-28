8f8223ba5903d4592c80a7c124357757435c0d21
rk-techs <ryskdevelop@gmail.com>
2024-01-28 09:35:49 +0900

    chore: install ESLint and plugin-react (#73)
    * chore: install eslint
    
    * Run `npm init @eslint/config`
    
    * chore: package.jsonにlint実行scripts設定
    
    * chore: 不要なrulesをOFFに設定
    
    * React 17以降で不要なルール
    * propTypesによる型チェックはしない
    
    * chore: gloabalsにrouteを追加
    
    * Ziggyパッケージで使用するrouteをグローバル変数として認識させる

---
+++ src/.eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "route": "readonly"
+    },
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope": 0,
+        "react/jsx-uses-react": 0,
+        "react/prop-types": 0,
+    }
+}
